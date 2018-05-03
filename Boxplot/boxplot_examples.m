7e1deea9e576ec0faab1d51bd69be8c83bd22c3d
Ahmed Abdul Quadeer <ahmedaq@gmail.com>
2018-05-03 20:12:36 +0800

    Add files via upload

--- Boxplot/boxplot_examples.m
+++ Boxplot/boxplot_examples.m
@@ -5,7 +5,7 @@ clear all; close all; clc
 num = 8;
 data = cell(1,num);
 for kk = 1:num
-    data{kk} = randi(10) + randn(1,500);
+    data{kk} = randi(10) + randn(randi([300 400]),1);
 end
 
 figure;figure_boxplot(data);
@@ -26,3 +26,23 @@ figure;figure_boxplot(data,label_axes,label_boxes);
 
 % set(gcf,'PaperUnits','centimeters','PaperPosition',[0 0 16 10])
 % print('boxplot2','-dpng','-r300')
+
+%% Example 3: Input data in matrix form (MxN)
+
+% N variables, M observations, and boxplot of each variable (column) is
+% required to be plotted
+
+% Number of intended boxes in the figure
+num_boxes = 7;    
+num_samples = 1000;
+
+% Generating random data
+data = zeros(num_samples,num_boxes);   
+for k = 1:num_boxes
+    data(:,k) = randi(10) + randn(num_samples,1);
+end
+
+figure;figure_boxplot(data);
+
+% set(gcf,'PaperUnits','centimeters','PaperPosition',[0 0 16 10])
+% print('boxplot3','-dpng','-r300')
