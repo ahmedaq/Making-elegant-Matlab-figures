fb007b8da8dc5a8ff0aaba5cfe0def0762340f28
Ahmed Abdul Quadeer <ahmedaq@gmail.com>
2018-05-09 21:27:40 +0800

    Show syntax of taking cell labels input

--- Heatmap/figure_heatmap_examples.m
+++ Heatmap/figure_heatmap_examples.m
@@ -34,15 +34,17 @@ figure_heatmap(C,colorscheme,text_title,text_labels);
 set(gcf,'PaperUnits','centimeters','PaperPosition',[0 0 16 12])
 print('heatmap_example3','-dpng','-r300')
 
-%% Example 4: Heatmap with limits of colorscheme specified
+%% Example 4: Heatmap with cell labels and limits of colorscheme specified
 
 colorscheme = 'BuGn';
 text_title = 'Correlation Matrix';
 text_labels = {'Variable','Variable'};
 limits_data = [-1 1]; %for correlation matrix
+text_labels_cells{1} = {'A','B','C','D','E'}; %x-axis cell labels
+text_labels_cells{2} = {'A','B','C','D','E'}; %y-axis cell labels
 
 % Heatmap figure
-figure_heatmap(C,colorscheme,text_title,text_labels,limits_data);
+figure_heatmap(C,colorscheme,text_title,text_labels,limits_data,text_labels_cells);
 
 set(gcf,'PaperUnits','centimeters','PaperPosition',[0 0 16 12])
 print('heatmap_example4','-dpng','-r300')
@@ -52,41 +54,48 @@ print('heatmap_example4','-dpng','-r300')
 % Generating rectangular matrix data
 X = randn(10,6);
 colorscheme = 'YlGnBu';
-text_title = 'Rectangular Matrix';
+text_title = 'Tall Matrix';
 text_labels = {'Variable 1','Variable 2'};
 limits_data = [floor(min(X(:))) ceil(max(X(:)))];
+text_labels_cells{1} = 1:size(X,2); %x-axis cell labels
+text_labels_cells{2} = 1:size(X,1); %y-axis cell labels
 savefig = 1;
 savefig_name = 'heatmap_example5a';
 
 % Heatmap figure
-figure_heatmap(X,colorscheme,text_title,text_labels,limits_data,...
+figure_heatmap(X,colorscheme,text_title,text_labels,limits_data,text_labels_cells,...
     savefig,savefig_name);
 
 %
 Y = randn(11,20);
 colorscheme = 'BuPu';
-text_title = 'Rectangular Matrix';
+text_title = 'Fat Matrix';
 text_labels = {'Variable 1','Variable 2'};
 limits_data = [floor(min(X(:))) ceil(max(X(:)))];
+text_labels_cells{1} = 1:size(Y,2); %x-axis cell labels
+text_labels_cells{2} = 1:size(Y,1); %y-axis cell labels
 savefig = 1;
 savefig_name = 'heatmap_example5b';
 
 % Heatmap figure
-figure_heatmap(Y,colorscheme,text_title,text_labels,limits_data,...
+figure;
+figure_heatmap(Y,colorscheme,text_title,text_labels,limits_data,text_labels_cells,...
     savefig,savefig_name);
 
 %% Example 6: Heatmap of a big data matrix 
 
 % Generating rectangular matrix data
-X = randn(15,40);
+X = randn(15,50);
 colorscheme = 'BuPu';
 text_title = 'Big Rectangular Matrix';
 text_labels = {'Variable 1','Variable 2'};
 limits_data = [floor(min(X(:))) ceil(max(X(:)))];
+text_labels_cells{1} = 1:size(X,2); %x-axis cell labels
+text_labels_cells{2} = 1:size(X,1); %y-axis cell labels
 savefig = 1;
 savefig_name = 'heatmap_example6';
 
 % Heatmap figure
-figure_heatmap(X,colorscheme,text_title,text_labels,limits_data,...
+figure_heatmap(X,colorscheme,text_title,text_labels,limits_data,text_labels_cells,...
     savefig,savefig_name);
 
